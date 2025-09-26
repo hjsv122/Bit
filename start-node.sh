55cc154463f4985b83361c1c8c3582b51935d573
hjsv122 <lwyalftal@gmail.com>
2025-09-26 04:59:30 +0300

    Update start-node.sh

--- start-node.sh
+++ start-node.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-geth --datadir ./batman-node init ./genesis.json
-geth --datadir ./batman-node --networkid 2025 --http --http.addr 0.0.0.0 --http.port 8545 --http.api personal,eth,net,web3,txpool --allow-insecure-unlock --nodiscover --miner.threads=1 --mine --syncmode full
+
+geth --networkid 2025 \
+  --http --http.addr 0.0.0.0 --http.port 8545 --http.api eth,net,web3,personal \
+  --http.corsdomain "*" \
+  --datadir "./data" \
+  init genesis.json
+
+geth --networkid 2025 \
+  --http --http.addr 0.0.0.0 --http.port 8545 --http.api eth,net,web3,personal \
+  --http.corsdomain "*" \
+  --datadir "./data" \
+  --allow-insecure-unlock \
+  --nodiscover \
+  --maxpeers 0 \
+  --mine \
+  --miner.threads=1 \
+  console
